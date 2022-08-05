2265dcf536a9e5dc685b1fbcea514a2a52eff4b5
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-05 20:14:45 -0300

    Create Database instance

---
+++ src/database/knex.ts
@@ -0,0 +1,15 @@
+import knex, { Knex } from "knex";
+require("dotenv").config();
+const config = {
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+};
+const client: Knex = knex(config as Knex.Config);
+
+export default client;
