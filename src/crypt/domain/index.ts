9020c8e7220c638a3d8e386348e18c679f409d9b
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-06 15:14:32 -0300

    Added compare passwords function to validate passwords

--- src/crypt/domain/index.ts
+++ src/crypt/domain/index.ts
@@ -5,6 +5,11 @@ const encryptPassword = async (password: string) => {
   return await cryptRepository.encryptPassword(password);
 };
 
+const comparePassword = async (userPassword: string, toCompare: string) => {
+  if (!userPassword || !toCompare) return Promise.resolve(false);
+  return await cryptRepository.comparePassword(userPassword, toCompare);
+};
+
 const generateJWT = async (uid: number, name: string) => {
   if (uid < 0) return Promise.resolve(null);
   return await cryptRepository.generateJWT(uid, name);
@@ -13,4 +18,5 @@ const generateJWT = async (uid: number, name: string) => {
 export const crypt = {
   encryptPassword,
   generateJWT,
+  comparePassword,
 };
