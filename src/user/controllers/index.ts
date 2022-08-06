7d753d4b4c2aa2e86f4825a813acb0c6022c3c69
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-06 15:16:10 -0300

    Added controller to login and renew the JWT

--- src/user/controllers/index.ts
+++ src/user/controllers/index.ts
@@ -2,6 +2,47 @@ import express from "express";
 import { crypt } from "../../crypt/domain";
 import { User } from "../domain";
 
+const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const userFinded = await User.find(req.body.email);
+    if (!userFinded.length) {
+      return res.status(401).json({
+        status: "Error",
+        ErrorMessage: "Email and password doesn't match to an user",
+      });
+    }
+
+    const isPassword = await crypt.comparePassword(
+      req.body.password,
+      userFinded[0].password
+    );
+
+    if (!isPassword) {
+      return res.status(401).json({
+        status: "Error",
+        ErrorMessage: "Email and password doesn't match to an user",
+      });
+    }
+
+    const token = await crypt.generateJWT(
+      userFinded[0].iduser,
+      userFinded[0].username
+    );
+
+    res.status(201).json({
+      uid: userFinded[0].iduser,
+      username: userFinded[0].username,
+      token: token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "Error",
+      ErrorMessage: "Please contact an admin.",
+    });
+  }
+};
+
 const create = async (req: express.Request, res: express.Response) => {
   try {
     const userFinded = await User.find(req.body.email);
@@ -35,6 +76,28 @@ const create = async (req: express.Request, res: express.Response) => {
   }
 };
 
+const renewToken = async (req: express.Request, res: express.Response) => {
+  const { uid, name } = req.body;
+  try {
+    const token = await crypt.generateJWT(uid, name);
+
+    return res.status(200).json({
+      status: "Completed",
+      token,
+      uid,
+      name,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: "Error",
+      ErrorMessage: "Please contact an admin",
+    });
+  }
+};
+
 export const UserController = {
   create,
+  login,
+  renewToken,
 };
