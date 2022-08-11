8ecd9444ef76a70709fce56f5f3fd61c11febef3
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-11 19:10:33 -0300

    Add Earning Route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import earning from "./earnings/controllers/route";
+import { Middlewares } from "./middlewares";
 import user from "./user/controllers/route";
 require("dotenv").config();
 
@@ -7,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/user", user);
+app.use("/api/earning", Middlewares.validateJWT, earning);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on ${process.env.PORT}`);
