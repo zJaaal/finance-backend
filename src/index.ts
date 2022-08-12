7e61c7cdf43efabdbc16e542df1886ee3d182072
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-12 17:37:28 -0300

    Add documentation to Index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ import expense from "./expenses/controllers/route";
 import user from "./user/controllers/route";
 import { Middlewares } from "./middlewares";
 
+//Initialize .env
 require("dotenv").config();
 
+//Initialize express
 const app = express();
 
+//Initialize json parser
 app.use(express.json());
 
-app.use("/api/user", user);
-app.use("/api/earning", Middlewares.validateJWT, earning);
-app.use("/api/expense", Middlewares.validateJWT, expense);
+//Initialize app endpoints
+app.use("/api/user", user); //User
+app.use("/api/earning", Middlewares.validateJWT, earning); //Earnings
+app.use("/api/expense", Middlewares.validateJWT, expense); //Expenses
 
+//Start to listen to server port
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on ${process.env.PORT}`);
 });
