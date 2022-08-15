1255daddc3e8e85326ade88de1bb1fa1ce369311
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-15 18:03:37 -0300

    Add unit testing for Expenses Module

---
+++ src/expenses/__test__/expenses.test.ts
@@ -0,0 +1,103 @@
+import { DateUtils } from "../../utils/date";
+import { Expenses } from "../domain";
+import { Expense, ExpenseIds } from "../validations/types";
+const should = require("chai").should();
+const expect = require("chai").expect;
+
+const keyword = "expense";
+const date = "8-15-2022";
+const expenseIdsToUpdate: ExpenseIds = {
+  iduser: 9,
+  idexpense: 21,
+};
+const expenseIdsToDelete: ExpenseIds = {
+  iduser: 9,
+  idexpense: 24,
+};
+const expensePut: Expense = {
+  idexpense: 21,
+  iduser: 9,
+  title: "Test Expense change",
+  description: "This was changed again",
+  date: date,
+  amount: Date.now(),
+};
+const expensePost: Expense = {
+  idexpense: undefined,
+  iduser: 9,
+  title: "Test Expense",
+  description: "This is a test Expense",
+  date: "08-15-2022",
+  amount: 1000,
+};
+
+describe("Expenses Module", () => {
+  it("Should create one expense", async () => {
+    const newExpense = await Expenses.create(expensePost);
+    newExpense.should.have.property("idexpense");
+  }),
+    it("Should update one expense from de DB", async () => {
+      const expenseToEdit = await Expenses.find(expenseIdsToUpdate);
+      await Expenses.update(expensePut);
+      const editedExpense = await Expenses.find(expenseIdsToUpdate);
+      expect(
+        expenseToEdit,
+        "Please be sure to fill editedExpense with different data"
+      ).to.be.deep.not.equal(editedExpense);
+    }),
+    it("Should list all expenses per page for one user ID", async () => {
+      const expenses = await Expenses.listPerPage(9, 1);
+      expect(expenses.every((expense) => expense.iduser === 9)).to.be.true;
+    }),
+    it("Should list all expenses per page for one user ID and from a specific date", async () => {
+      const expenses = await Expenses.listPerPage(9, 1, undefined, date);
+      expect(
+        expenses.every(
+          (expense) =>
+            expense.iduser === 9 &&
+            DateUtils.getDateString(expense.date) == date
+        )
+      ).to.be.true;
+    }),
+    it("Should list all expenses per page for one user ID and with an specific keyword", async () => {
+      const expenses = await Expenses.listPerPage(9, 1, keyword, undefined);
+      expect(
+        expenses.every(
+          (expense) =>
+            expense.iduser === 9 &&
+            (expense.title.toLowerCase().includes(keyword) ||
+              expense.description.toLowerCase().includes(keyword))
+        )
+      ).to.be.true;
+    }),
+    it("Should list all expenses per page for one user ID, with an specific keyword and from a specific date", async () => {
+      const expenses = await Expenses.listPerPage(9, 1, keyword, date);
+      expect(
+        expenses.every(
+          (expense) =>
+            expense.iduser === 9 &&
+            (expense.title.toLowerCase().includes(keyword) ||
+              expense.description.toLowerCase().includes(keyword)) &&
+            DateUtils.getDateString(expense.date) == date
+        )
+      ).to.be.true;
+    }),
+    it("Should return an empty array for an user that doesn't exist", async () => {
+      const expenses = await Expenses.listPerPage(12312321, 1);
+      expect(expenses).to.be.empty;
+    }),
+    it("Should delete an existing expense", async () => {
+      const expenseToDelete = await Expenses.find(expenseIdsToDelete);
+      if (expenseToDelete) {
+        await Expenses.erase(expenseIdsToDelete);
+        const deletedExpense = await Expenses.find(expenseIdsToDelete);
+
+        expect(deletedExpense).to.not.exist;
+      } else {
+        expect(
+          expenseToDelete,
+          "Please be sure that the expenseIdsToDelete are valid IDs"
+        ).to.exist;
+      }
+    });
+});
